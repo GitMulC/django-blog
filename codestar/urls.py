c8291206eec1446cae2a3193a24bdee4678bc56c
GitMulC <cmulvo@gmail.com>
2022-04-28 16:38:10 +0000

    Add main views

--- codestar/urls.py
+++ codestar/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('summer_note/', include('django_summernote.urls')),
+    path('', include('blog.urls', name='blog_urls'))
 ]
